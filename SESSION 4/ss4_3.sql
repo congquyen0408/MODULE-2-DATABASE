e3ae2132816d3e31868d6c88d5b382e2ebdc0d4a
Le Chau Cong Quyen BRSE NI - 09 <congquyen0408@gmail.com>
2026-08-18 19:51:37 +0900

    Add files via upload

---
+++ SESSION 4/ss4_3.sql
@@ -0,0 +1,52 @@
+CREATE DATABASE student_manage_database;
+
+USE student_manage_database;
+
+CREATE TABLE students (
+student_id VARCHAR(10) PRIMARY KEY,
+full_name VARCHAR(100) NOT NULL,
+birth_date DATE,
+gender ENUM("male", "female", "other") NOT NULL,
+email VARCHAR(100)
+);
+
+SELECT * FROM student_manage_database.students;
+
+INSERT INTO students (student_id, full_name, birth_date, gender, email)
+VALUES 
+('SV001', 'Nguyễn Văn A', '2000-01-20', 'male', 'nguyenvana@gmail.com'),
+('SV002', 'Nguyễn Thị B','2003-02-01', 'female', 'nguyenthib@gmail.com'),
+('SV003', 'Nguyễn Văn C','1998-08-15', 'other', NULL),
+('SV004', 'Lê Hoàng D','2002-06-25', 'male', 'lehoangd@gmail.com'),
+('SV005', 'Võ Thị E','2005-08-18', 'male', 'vothie@gmail.com');
+
+SELECT * FROM student_manage_database.students;
+
+SELECT student_id, full_name, email 
+FROM students;
+
+UPDATE students
+SET email = 'nguyenvanc@gmail.com'
+WHERE email IS NULL;
+SELECT * FROM students;
+
+UPDATE students
+SET gender = 'other'
+WHERE student_id = 'SV005';
+SELECT * FROM students;
+
+DELETE FROM students
+WHERE student_id = 'SV003';
+SELECT * FROM students;
+
+SELECT student_id, full_name, birth_date 
+FROM students
+WHERE YEAR(birth_date) BETWEEN 2003 AND 2005;
+
+SELECT student_id, full_name, birth_date 
+FROM students
+WHERE gender IN ('male', 'female');
+
+SELECT student_id, full_name, birth_date 
+FROM students
+WHERE student_id IN ('SV001', 'SV004', 'SV005');
