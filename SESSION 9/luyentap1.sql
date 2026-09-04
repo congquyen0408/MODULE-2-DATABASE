b9a1797f32daad58d8f6b4ce4c3a97d12aa38f99
Le Chau Cong Quyen BRSE NI - 09 <congquyen0408@gmail.com>
2026-09-04 23:33:07 +0900

    Add files via upload

---
+++ SESSION 9/luyentap1.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE session_9;
+USE session_9;
+
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone VARCHAR(15) NOT NULL,
+    address VARCHAR(255) NOT NULL
+);
+
+CREATE UNIQUE INDEX index_unique_email ON customers(email);
+
+CREATE INDEX index_phone ON customers(phone);
